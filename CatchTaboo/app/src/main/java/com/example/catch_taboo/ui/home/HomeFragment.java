a83cc4669c48ba67a7f6753e5320eb1bac53674f
Elly Landrum <elly.landrum@colorado.edu>
2020-02-22 11:46:00 -0700

    Firebase Connected

--- CatchTaboo/app/src/main/java/com/example/catch_taboo/ui/home/HomeFragment.java
+++ CatchTaboo/app/src/main/java/com/example/catch_taboo/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.catch_taboo.ui.home;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +12,16 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.catch_taboo.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class HomeFragment extends Fragment {
 
@@ -27,7 +36,15 @@ public class HomeFragment extends Fragment {
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("users").document("WqGIGKODr6yQOxYH2bu8")
+                        .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String name = documentSnapshot.getString("name");
+                        textView.setText(name);
+                    }
+                });
             }
         });
         return root;
