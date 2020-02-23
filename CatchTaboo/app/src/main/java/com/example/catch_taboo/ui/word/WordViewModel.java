3db1f21673ea5efecb76fa0d6cc0d9389c32e67d
JosephineMartin <joma1927@colorado.edu>
2020-02-23 13:05:40 -0700

    add word to create game

--- CatchTaboo/app/src/main/java/com/example/catch_taboo/ui/word/WordViewModel.java
+++ CatchTaboo/app/src/main/java/com/example/catch_taboo/ui/word/WordViewModel.java
@@ -33,7 +33,7 @@ import java.util.Random;
 import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class WordViewModel extends ViewModel {
-    private int number = 10;
+    private int number = 9;
     public WordViewModel(String mGameName){
         Log.v("GameName: ", mGameName);
         gameName = mGameName;
