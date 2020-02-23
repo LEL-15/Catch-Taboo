789abc648b53d0b1398e0efff2805795a0ed3c3e
Elly Landrum <elly.landrum@colorado.edu>
2020-02-22 22:49:27 -0700

    Pick Random Player

--- CatchTaboo/app/src/main/java/com/example/catch_taboo/JoinedGameActivity.java
+++ CatchTaboo/app/src/main/java/com/example/catch_taboo/JoinedGameActivity.java
@@ -10,6 +10,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
@@ -18,25 +19,32 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class JoinedGameActivity extends AppCompatActivity {
     String TAG = "testing";
 
     private AppBarConfiguration mAppBarConfiguration;
-    FirebaseFirestore rootRef = FirebaseFirestore.getInstance();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     FirebaseAuth mAuth;
     private GoogleSignInClient mGoogleSignInClient;
+    private int changes = 0;
+    private int teamChanges = 0;
+    private Boolean first;
+    private String gameName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +52,9 @@ public class JoinedGameActivity extends AppCompatActivity {
         final Context hold = this;
         Intent loadIntent = getIntent();
         //Find out what the game's id is
-        String id = loadIntent.getStringExtra("ID");
-        Log.d(TAG, "onCreate: id is" + id);
+        gameName = loadIntent.getStringExtra("ID");
         setContentView(R.layout.activity_joined_game);
-        DocumentReference game = rootRef.collection("games").document(id);
+        DocumentReference game = db.collection("games").document(gameName);
         CollectionReference team1 = game.collection("team1");
         team1.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
@@ -114,6 +121,108 @@ public class JoinedGameActivity extends AppCompatActivity {
                 String gameNameString = documentSnapshot.getString("name");
                 gameName.setText(gameNameString);
 
+            }
+        });
+        DocumentReference docRef = db.collection("games").document(gameName);
+        docRef.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot,
+                                @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                String source = snapshot != null && snapshot.getMetadata().hasPendingWrites()
+                        ? "Local" : "Server";
+                if (snapshot != null && snapshot.exists()) {
+                    gameValueChanged(snapshot.getData());
+                } else {
+                    Log.d(TAG, source + " data: null");
+                }
+            }
+        });
+        Query query = db.collection("games").document(gameName).collection("team1");
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            // [START_EXCLUDE]
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshots,
+                                @Nullable FirebaseFirestoreException e) {
+                teamValueChanged();
+            }
+        });
+        query = db.collection("games").document(gameName).collection("team2");
+        query.addSnapshotListener(new EventListener<QuerySnapshot>() {
+            // [START_EXCLUDE]
+            @Override
+            public void onEvent(@Nullable QuerySnapshot snapshots,
+                                @Nullable FirebaseFirestoreException e) {
+                teamValueChanged();
+            }
+        });
+    }
+    public void gameValueChanged(Map<String, Object> game) {
+        changes+=1;
+        //You just loaded the page
+        if(changes == 1){
+            first = Boolean.parseBoolean(game.get("teamOneActive").toString());
+        }
+        else{
+            //Somebody started the game
+            if(Boolean.parseBoolean(game.get("teamOneActive").toString()) != first){
+                Intent intent = new Intent(this, GeneralPlayActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+    public void teamValueChanged() {
+        teamChanges+=1;
+        //You just loaded the page
+        if(teamChanges > 2){
+            Intent intent = new Intent(this, JoinedGameActivity.class);
+            intent.putExtra("ID", gameName);
+            startActivity(intent);
+        }
+    }
+    public void startGame(View v){
+        final Context hold = this;
+        //Switch which team goes first
+        final Map<String, Object> data = new HashMap<>();
+        data.put("teamOneActive", !first);
+        //Switch active player
+        String teamFirst = "team2";
+        if(first){
+            teamFirst = "team1";
+        }
+        CollectionReference team2 = db.collection("games").document(gameName).collection(teamFirst);
+        team2.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                //If succesfully accessed firebase
+                if (task.isSuccessful()) {
+                    // create instance of Random class
+                    Random rand = new Random();
+                    // Generate random integers in range 0 to 999
+                    int randomPlayer = rand.nextInt(task.getResult().size());
+                    Log.d(TAG, "random player: " + randomPlayer);
+                    //For every plyaer in the database
+                    int count = 0;
+                    for (final QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, "onComplete: per player");
+                        if(count == randomPlayer){
+                            Log.d(TAG, "onComplete: found right player with id " + document.get("id"));
+                            data.put("activePlayer", document.get("id"));
+                        }
+                        count += 1;
+                    }
+                    Log.d(TAG, "data is " + data);
+                    DocumentReference game = db.collection("games").document(gameName);
+                    game.update(data);
+                }
+                //If failed to access firebase
+                else {
+                    Log.d("Testing", "Problem");
+                }
             }
         });
     }
